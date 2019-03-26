414daf26202667f25f7b0711fc798b2f05c537fe
Herman <herman@dompseler.nl>
2019-03-26 16:59:17 +0100

    only deploy image, rely on jenkins to provide the image

--- prepare.sh
+++ prepare.sh
@@ -3,9 +3,6 @@
 # todo
 # - two variables: network + containername
 
-# get image
-docker pull 192.87.106.18:56001/helloworld:0.1
-
 # create nfs mount
 mkdir -p /mnt/nfs/nfsdlo/dev-net/helloword-0.1/htdocs
 
