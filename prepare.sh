e41621567df480c3e79b94f4a3a10f6f20ffc38e
Herman <herman@dompseler.nl>
2019-03-26 16:36:05 +0100

    store data on nfs

--- prepare.sh
+++ prepare.sh
@@ -8,3 +8,6 @@ docker pull 192.87.106.18:56001/helloworld:0.1
 
 # create nfs mount
 mkdir -p /mnt/nfs/nfsdlo/dev-net/helloword-0.1/htdocs
+
+# write data
+cp -a ./htdocs/* /mnt/nfs/nfsdlo/dev-net/helloword-0.1/htdocs
